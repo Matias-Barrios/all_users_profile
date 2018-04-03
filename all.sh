d562bb54acf7969d6553ea31ce68b82bcac69f90
matias <soymatiasbarrios@gmail.com>
2018-04-03 16:40:28 -0300

    init

---
+++ all.sh
@@ -0,0 +1,14 @@
+clear
+echo "**************************************"
+echo "*                                    *"
+echo "*     Bienvenido!!                   *"
+echo "*    Este es el servidor             *"
+echo "*       de Matias-Barrios            *"
+echo "*  \"No te olvides de poner el        *"
+echo "*      WHERE en el DELETE FROM\"      *"
+echo "*                                    *"
+echo "**************************************"
+
+
+
+
